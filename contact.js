86e71733d65d960dd1fda3182769bad727a03878
lebenpdx <116242222+lebenpdx@users.noreply.github.com>
2023-06-07 16:41:02 -0700

    added email field to contact

--- contact.js
+++ contact.js
@@ -4,9 +4,11 @@ form.addEventListener("submit", function (event) {
   event.preventDefault();
 
   const name = document.querySelector("#name").value;
+  const email = document.querySelector("#email").value;
   const msg = document.querySelector("#msg").value;
 
   console.log("Name: " + name);
+  console.log("E-mail: " + email);
   console.log("Message: " + msg);
 
   form.reset();
